3b2e85709c70c1445639849dd71f9ed65a8ca6ea
Andrew Hill <ahill@rocketcyber.com>
2019-06-05 15:18:22 -0500

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   # This means a GET on .../static_pages/home goes to StaticPagesController method "home"
-  get 'static_pages/home'
+  # root 'static_pages/home'
   # As above, to "help"
+  get 'static_pages/home'
   get 'static_pages/help'
   get 'static_pages/about'
+  get 'static_pages/contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'application#hello'
+  root 'static_pages#home'
 end
