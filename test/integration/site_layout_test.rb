e40f971cda54e9ff846e3e5e1f8f9f1580e554ca
Andrew Hill <ahill@rocketcyber.com>
2019-06-06 12:03:20 -0500

    retry commit

---
+++ test/integration/site_layout_test.rb
@@ -0,0 +1,20 @@
+require 'test_helper'
+
+class SiteLayoutTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+  test "layout links" do
+    get root_path
+    assert_template 'static_pages/home'
+    assert_select "a[href=?]", root_path
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", signup_path, count: 2
+    # inserts parameter (e.g. contact_path) in place of "?" and checks whether the assert is true
+
+    get contact_path
+    assert_select "title", full_title("Contact")
+  end
+end
